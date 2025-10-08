d4989dac7c7d7f57843e85fa37d37b2e10ddb0e2
rbabayev <rufat.babayev@soprasteria.com>
2025-10-08 15:22:15 +0200

    first commit: frontend folder with reactjs + tailwindcss and backend folder with spring boot

---
+++ backend/pid-rbabayev/src/main/java/com/example/pid_rbabayev/PidRbabayevApplication.java
@@ -0,0 +1,13 @@
+package com.example.pid_rbabayev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidRbabayevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidRbabayevApplication.class, args);
+	}
+
+}
---
+++ backend/pid-rbabayev/src/test/java/com/example/pid_rbabayev/PidRbabayevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pid_rbabayev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidRbabayevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
