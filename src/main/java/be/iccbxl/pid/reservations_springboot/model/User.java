6a9209d6b7fb7ebfb1e89e254f6c9da4672c7e83
Mepho9 <babayev.r@outlook.com>
2025-12-02 21:05:55 +0100

    Relation ManyToMany between User & Role

--- src/main/java/be/iccbxl/pid/reservations_springboot/model/User.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/model/User.java
@@ -13,40 +13,114 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 @Entity
 @Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-
-    @Column(unique = true, nullable = false)
     private String login;
-
-    @Column(nullable = false)
     private String password;
-
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
-
     private String firstname;
     private String lastname;
-
-    @Column(unique = true, nullable = false)
     private String email;
-    
     private String langue;
-    
-    @Enumerated(EnumType.STRING)
-    private UserRole role;
-    
-    private LocalDateTime created_at = LocalDateTime.now();
+    private LocalDateTime created_at;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Role> roles = new ArrayList<>();
+
+    protected User() {}
+
+    public User(String login, String firstname, String lastname) {
+        this.login = login;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.created_at = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLangue() {
+        return langue;
+    }
+
+    public void setLangue(String langue) {
+        this.langue = langue;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public User addRole(Role role) {
+        if(!this.roles.contains(role)) {
+            this.roles.add(role);
+            role.addUser(this);
+        }
+
+        return this;
+    }
+
+    public User removeRole(Role role) {
+        if(this.roles.contains(role)) {
+            this.roles.remove(role);
+            role.getUsers().remove(this);
+        }
+
+        return this;
+    }
 
     @Override
     public String toString() {
-        return login + " (" + firstname + " " + lastname + " - " + role + ")";
+        return login + "(" + firstname + " " + lastname + ")";
     }
 }
+
 
