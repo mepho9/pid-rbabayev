70750beb99ed44d31797e78c0a283c609c3d288b
Mepho9 <babayev.r@outlook.com>
2025-12-02 20:41:08 +0100

    Modèle Show +Relation ManyToOne with Location

--- src/main/java/be/iccbxl/pid/reservations_springboot/model/Location.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/model/Location.java
@@ -31,6 +31,10 @@ public class Location {
     private String website;
     private String phone;
 
+    @OneToMany(targetEntity=Show.class, mappedBy="location")
+    private List<Show> shows = new ArrayList<>();
+
+
     protected Location() { }
 
     public Location(String slug, String designation, String address, Locality locality, String website, String phone) {
@@ -107,5 +111,35 @@ public class Location {
         return "Location [id=" + id + ", slug=" + slug + ", designation=" + designation
                 + ", address=" + address	+ ", locality=" + locality + ", website="
                 + website + ", phone=" + phone + "]";
+    }
+    public List<Show> getShows() {
+        return shows;
+    }
+
+    public Location addShow(Show show) {
+        if(!this.shows.contains(show)) {
+            this.shows.add(show);
+            show.setLocation(this);
+        }
+
+        return this;
+    }
+
+    public Location removeShow(Show show) {
+        if(this.shows.contains(show)) {
+            this.shows.remove(show);
+            if(show.getLocation().equals(this)) {
+                show.setLocation(null);
+            }
+        }
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Location [id=" + id + ", slug=" + slug + ", designation=" + designation
+                + ", address=" + address	+ ", locality=" + locality + ", website="
+                + website + ", phone=" + phone + ", shows=" + shows.size() + "]";
     }
 }
---
+++ src/main/java/be/iccbxl/pid/reservations_springboot/repository/ShowRepository.java
@@ -0,0 +1,12 @@
+package be.iccbxl.pid.reservations_springboot.repository;
+
+import java.util.List;
+
+import be.iccbxl.pid.reservations_springboot.model.Show;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ShowRepository extends CrudRepository<Show, Long> {
+    Show findBySlug(String slug);
+    Show findByTitle(String title);
+    List<Show> findByLocation(Location location);
+}
