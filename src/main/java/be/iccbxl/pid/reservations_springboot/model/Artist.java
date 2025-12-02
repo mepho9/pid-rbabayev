e05478c748f6b7642decb74e61406568ce61f4a6
Mepho9 <babayev.r@outlook.com>
2025-12-02 20:57:24 +0100

    Relation ManyToMany between Artist & Type

--- src/main/java/be/iccbxl/pid/reservations_springboot/model/Artist.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/model/Artist.java
@@ -10,8 +10,12 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor(force = true, access = AccessLevel.PUBLIC)
+
 @Entity
 @Table(name="artists")
 public class Artist {
@@ -21,4 +25,60 @@ public class Artist {
 	private String firstname;
 	private String lastname;
 
+	@ManyToMany(mappedBy = "artists")
+	private List<Type> types = new ArrayList<>();
+
+	protected Artist() {}
+
+	public Artist(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public List<Type> getTypes() {
+		return types;
+	}
+
+	public Artist addType(Type type) {
+		if(!this.types.contains(type)) {
+			this.types.add(type);
+			type.addArtist(this);
+		}
+
+		return this;
+	}
+
+	public Artist removeType(Type type) {
+		if(this.types.contains(type)) {
+			this.types.remove(type);
+			type.getArtists().remove(this);
+		}
+
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return firstname + " " + lastname;
+	}
 }
--- src/main/java/be/iccbxl/pid/reservations_springboot/model/Type.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/model/Type.java
@@ -1,27 +1,47 @@
 package be.iccbxl.pid.reservations_springboot.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="types")
 public class Type {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String type;
 
-    protected Type() { }
+    @ManyToMany
+    @JoinTable(
+            name = "artist_type",
+            joinColumns = @JoinColumn(name = "type_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id"))
+    private List<Artist> artists = new ArrayList<>();
 
-    public Type(String type) {
+    public Type() {
+    }
+
+    public Type(Long id, String type) {
+        super();
+        this.id = id;
         this.type = type;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getType() {
@@ -32,8 +52,32 @@ public class Type {
         this.type = type;
     }
 
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public Type addArtist(Artist artist) {
+        if(!this.artists.contains(artist)) {
+            this.artists.add(artist);
+            artist.addType(this);
+        }
+
+        return this;
+    }
+
+    public Type removeType(Artist artist) {
+        if(this.artists.contains(artist)) {
+            this.artists.remove(artist);
+            artist.getTypes().remove(this);
+        }
+
+        return this;
+    }
+
     @Override
     public String toString() {
         return "Type [id=" + id + ", type=" + type + "]";
     }
+
+
 }
